1035aa4234e5610879f5f312e0080e69049c3521
Amal Jose <amalavirappatt@yahoo.com>
2020-10-17 13:47:30 +0530

    add data

--- server/index.js
+++ server/index.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const admin = require('firebase-admin')
 const serviceAccount = require('./config')
+
 const app = express()
+
+app.use(express.urlencoded({extended: false}))
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount.serviceAccountKey)
 })
+
+const db = admin.firestore()
+
+app.post('/addswag', async(req, res) => {
+    const { name, description, url, imgUrl } = req.body
+
+    const newSwag = {
+        name: name,
+        description: description,
+        url: url,
+        imgUrl: imgUrl,
+        createdAt: new Date()
+    }
+
+    const result = await db.collection('swagDetails').add(newSwag)
+
+    res.send(`New swag added with swag id: ${result.id}`)
+})
+
+
 
 const PORT = 5000
 app.listen(PORT, () => {
