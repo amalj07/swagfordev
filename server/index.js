fbcc3ea18e2742b82883d4446202eb08489ed593
Amal Jose <amalavirappatt@yahoo.com>
2020-10-17 12:47:34 +0530

    add firebase

--- server/index.js
+++ server/index.js
@@ -1,10 +1,6 @@
 const express = require('express')
-const connectDb = require('./config/db')
 
 const app = express()
-
-// Connect to Database
-connectDb()
 
 const PORT = 5000
 app.listen(PORT, () => {
