3667f6a5ca59bbf9f1e9472e9d0a547a724ab27d
Amal Jose <amalavirappatt@yahoo.com>
2020-10-17 13:07:26 +0530

    init firebase

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
-
+const admin = require('firebase-admin')
+const serviceAccount = require('./config')
 const app = express()
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount.serviceAccountKey)
+})
 
 const PORT = 5000
 app.listen(PORT, () => {
