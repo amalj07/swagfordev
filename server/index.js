029e8b7c2767d6285a522409eccd6cb4cb947da7
Amal Jose <amalavirappatt@yahoo.com>
2020-10-18 15:33:49 +0530

    add dotenv

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('dotenv').config()
 const admin = require('firebase-admin')
 const cors = require('cors')
 const multer = require('multer')
@@ -12,7 +13,7 @@ app.use(express.urlencoded({extended: false}))
 // Initialize firebase admin SDK
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount.serviceAccountKey),
-    storageBucket: "swagfordevs.appspot.com"
+    storageBucket: process.env.STORAGEBUCKET
 })
 
 // firestore
@@ -99,7 +100,7 @@ app.post('/addswag', async(req, res) => {
 
 
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`🚀Server listening on port: ${PORT}`)
 })
