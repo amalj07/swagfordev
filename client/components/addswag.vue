2902495e2bd9a9cf287cc020a9f3525ae7da7f35
Amal Jose <amalavirappatt@yahoo.com>
2020-10-18 08:59:39 +0530

    upload file

--- client/components/addswag.vue
+++ client/components/addswag.vue
@@ -32,52 +32,66 @@
             <v-btn
               dark
               text
-              @click="dialog = false"
+              @click="addSwag"
             >
               Save
             </v-btn>
           </v-toolbar-items>
         </v-toolbar>
-        <v-list
-          three-line
-          subheader
-        >
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>Swag name</v-list-item-title>
-              <v-list-item-subtitle>
-                <v-text-field></v-text-field>
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>Swag details</v-list-item-title>
-              <v-list-item-subtitle>
-                <v-textarea
-                    rows="1"
-                    auto-grow
-                ></v-textarea>
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>Swag details url</v-list-item-title>
-              <v-list-item-subtitle>
-                <v-text-field></v-text-field>
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>Swag image</v-list-item-title>
-              <v-list-item-subtitle>
-                <v-file-input></v-file-input>
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
+        <v-form>
+          <v-container>
+            <v-row>
+              <v-col
+                class="d-flex"
+              >
+                <v-card
+                  class="mt-5"
+                  width=700px
+                  outlined
+                >
+                  <v-list>
+                    <v-list-item>
+                      <v-list-item-content>
+                        <v-list-item-title>Name</v-list-item-title>
+                        <v-text-field
+                          v-model="swag.name"
+                        ></v-text-field>  
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-list-item-content>
+                        <v-list-item-title>How to get?</v-list-item-title>
+                        <v-textarea
+                            v-model="swag.description"
+                            rows="1"
+                            auto-grow
+                        ></v-textarea>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-list-item-content>
+                        <v-list-item-title>URL</v-list-item-title>
+                        <v-text-field
+                          v-model="swag.url"
+                        ></v-text-field>  
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-list-item-content>
+                        <v-list-item-title>Image</v-list-item-title>
+                        <v-list-item-subtitle>
+                          <v-file-input
+                            v-model="swag.image"
+                          ></v-file-input>
+                        </v-list-item-subtitle>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-list>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-form> 
       </v-card>
     </v-dialog>
 </template>
@@ -86,8 +100,32 @@
 export default {
     data () {
         return {
-            dialog: false
+          dialog: false,
+          swag: {
+            name: "",
+            description: "",
+            url: "",
+            image: null
+          }
         }
+    },
+    methods: {
+      addSwag() {
+        const formData = new FormData()
+        formData.append("name", this.swag.name)
+        formData.append("description", this.swag.description)
+        formData.append("url", this.swag.url)
+        formData.append("image", this.swag.image)
+        this.$axios.$post('/addswag', formData, {
+          headers: {
+            "Content-Type": "multipart/form-data"
+          }
+        }).then(response => {
+          console.log(response)
+        }).catch(error => {
+          console.log(error.response.data)
+        })
+      }
     }
 }
 </script>
