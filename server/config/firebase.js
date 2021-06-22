7f897bbbae7c8b4e1af32f21828cb2cfc75f4d0b
Amal Jose <31209960+amljs@users.noreply.github.com>
2021-06-22 11:59:26 +0530

    minor fix

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin')
 
 // Initialize firebase admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert(require('./swagfordevs-firebase-adminsdk-cwq6s-3fb8c40c45.json')),
+  credential: admin.credential.cert(require('<key_loc>')),
   storageBucket: process.env.STORAGEBUCKET
 })
 
